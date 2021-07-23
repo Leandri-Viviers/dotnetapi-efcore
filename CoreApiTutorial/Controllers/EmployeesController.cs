1552bc39958a23e75e552289e76917b7a0192007
Leandri Viviers <leandri.viviers@entelect.co.za>
2021-07-23 22:23:39 +0200

    Add Swagger docs

--- CoreApiTutorial/Controllers/EmployeesController.cs
+++ CoreApiTutorial/Controllers/EmployeesController.cs
@@ -9,70 +9,98 @@ using System.Threading.Tasks;
 
 namespace CoreApiTutorial.Controllers
 {
-    [ApiController]
-    public class EmployeesController : ControllerBase
+  [ApiController]
+  public class EmployeesController : ControllerBase
+  {
+    private IEmployeeData employeeData;
+    public EmployeesController(IEmployeeData _employeeData)
     {
-        private IEmployeeData employeeData;
-        public EmployeesController(IEmployeeData _employeeData)
-        {
-            employeeData = _employeeData;
-        }
-
-        [HttpGet]
-        [Route("api/[controller]")]
-        public IActionResult GetEmployees()
-        {
-            return Ok(employeeData.GetEmployees());
-        }
-
-        [HttpGet]
-        [Route("api/[controller]/{id}")]
-        public IActionResult GetEmployee(Guid id)
-        {
-            var employee = employeeData.GetEmployee(id);
-
-            if(employee != null)
-            {
-                return Ok(employee);
-            }
-            return NotFound($"Employee with Id: {id} was not found.");
-        }
-
-        [HttpPost]
-        [Route("api/[controller]")]
-        public IActionResult AddEmployee(Employee employee)
-        {
-            employeeData.AddEmployee(employee);
-            return Created($"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}{HttpContext.Request.Path}/{employee.Id}", employee);
-        }
-
-        [HttpDelete]
-        [Route("api/[controller]/{id}")]
-        public IActionResult DeleteEmployee(Guid id)
-        {
-            var employee = employeeData.GetEmployee(id);
-
-            if(employee != null)
-            {
-                employeeData.DeleteEmployee(employee);
-                return Ok();
-            }
-            return NotFound($"Employee with Id: {id} was not found.");
-        }
-
-        [HttpPatch]
-        [Route("api/[controller]/{id}")]
-        public IActionResult UpdateEmployee(Guid id, Employee employee)
-        {
-            var result = employeeData.GetEmployee(id);
-
-            if (result != null)
-            {
-                employee.Id = result.Id;
-                employeeData.UpdateEmployee(employee);
-                return Ok(employee);
-            }
-            return NotFound($"Employee with Id: {id} was not found.");
-        }
+      employeeData = _employeeData;
     }
+
+    /// <summary>
+    /// Get all employees
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [ProducesResponseType(200)]
+    [Route("api/[controller]")]
+    public IActionResult GetEmployees()
+    {
+      return Ok(employeeData.GetEmployees());
+    }
+
+    /// <summary>
+    /// Get an employee by Id
+    /// </summary>
+    /// <param name="id"> Employee Guid to fetch </param>
+    [HttpGet]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    [Route("api/[controller]/{id}")]
+    public IActionResult GetEmployee(Guid id)
+    {
+      var employee = employeeData.GetEmployee(id);
+
+      if (employee != null)
+      {
+        return Ok(employee);
+      }
+      return NotFound($"Employee with Id: {id} was not found.");
+    }
+
+    /// <summary>
+    /// Add a new employee
+    /// </summary>
+    [HttpPost]
+    [Route("api/[controller]")]
+    [ProducesResponseType(201)]
+    public IActionResult AddEmployee(Employee employee)
+    {
+      employeeData.AddEmployee(employee);
+      return Created($"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}{HttpContext.Request.Path}/{employee.Id}", employee);
+    }
+
+    /// <summary>
+    /// Delete an employee
+    /// </summary>
+    /// <param name="id"> Employee Guid to remove </param>
+    [HttpDelete]
+    [Route("api/[controller]/{id}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public IActionResult DeleteEmployee(Guid id)
+    {
+      var employee = employeeData.GetEmployee(id);
+
+      if (employee != null)
+      {
+        employeeData.DeleteEmployee(employee);
+        return Ok();
+      }
+      return NotFound($"Employee with Id: {id} was not found.");
+    }
+
+    /// <summary>
+    /// Update an employee
+    /// </summary>
+    /// <param name="id"> Employee Guid to update </param>
+    /// <param name="employee"></param>
+    [HttpPatch]
+    [Route("api/[controller]/{id}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public IActionResult UpdateEmployee(Guid id, Employee employee)
+    {
+      var result = employeeData.GetEmployee(id);
+
+      if (result != null)
+      {
+        employee.Id = result.Id;
+        employeeData.UpdateEmployee(employee);
+        return Ok(employee);
+      }
+      return NotFound($"Employee with Id: {id} was not found.");
+    }
+  }
 }
--- CoreApiTutorial/Startup.cs
+++ CoreApiTutorial/Startup.cs
@@ -7,9 +7,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace CoreApiTutorial
@@ -26,6 +29,21 @@ namespace CoreApiTutorial
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Employee API",
+                    Version = "v1",
+                    Description = "An API to perform Employee operations"
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
             services.AddControllers();
 
             services.AddSingleton<IEmployeeData, MockEmployeeData>();
@@ -38,6 +56,15 @@ namespace CoreApiTutorial
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+            });
 
             app.UseHttpsRedirection();
 
